6ea17fd1b23dbeb5d2cf05f0b27b3730b8a0f7c5
scenesua <scenesua@gmail.com>
2026-08-18 22:56:55 +0900

    chore: bump prerelease version to 1.1.0-alpha.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.scene.ambience"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.1.0-alpha.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
