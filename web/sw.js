0a7a8b39521347374486934a55aea77a53f47116
scenesua <scenesua@gmail.com>
2026-08-19 15:22:19 +0900

    web: refresh PWA cache for player

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE='lullaby-scene-site-v1';
+const CACHE='lullaby-scene-site-v2';
 const APP_SHELL=['/','/styles.css','/app.js','/manifest.webmanifest','/assets/icon.svg','/privacy/','/terms/'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
